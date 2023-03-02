2f2c7d01c6f3976a649b0b6f8f8ea2926c92a9db
Gorgui24 <fayesouleymanegorgui@gmail.com>
2023-03-02 12:01:14 -0800

    Binary Tees

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -3,7 +3,7 @@
  * binary_tree_insert_right - Inserts a node as the right_child of another node
  * @parent: is a pointer to the node to insert the right-child in
  * @value: is the value to store in the new node
- * @Return: a pointer to the created node, or NULL on failure or if parent is NULL
+ * Return: a pointer to the created node, or NULL on failure or if parent is NULL
  */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
