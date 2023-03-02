eaf02ad282991ee19bcac24d225da454d80de9ca
Gorgui24 <fayesouleymanegorgui@gmail.com>
2023-03-01 16:45:49 -0800

    Binary Tree

---
+++ 1-binary_tree_insert_left.c
@@ -0,0 +1,24 @@
+#include "binary_trees.h"
+/**
+ * @parent is a pointer to the node to insert the left-child in
+ * @value is the value to store in the new node
+ * Return:  a pointer to the created node, or NULL on failure or if parent is NULL
+ */
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new;
+
+	if (!parent)
+		return (NULL);
+	new = binary_tree_node(parent, value);
+	if (!new)
+		return (NULL);
+
+	if (parent->left)
+	{
+		parent->left->parent = new;
+		new->left = parent->left;
+	}
+	parent->left = new;
+	return (new);
+}
---
+++ 2-binary_tree_insert_right.c
@@ -0,0 +1,24 @@
+#include "binary_trees.h"
+/**
+ * @parent is a pointer to the node to insert the right-child in
+ * @value is the value to store in the new node
+ * @Return: a pointer to the created node, or NULL on failure or if parent is NULL
+ */
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new;
+
+	if (!parent)
+		return (NULL);
+	new = binary_tree_node(parent, value);
+	if (!new)
+		return (NULL);
+
+	if (parent->right)
+	{
+		parent->right->parent = new;
+		new->right = parent->right;
+	}
+	parent->right = new;
+	return (new);
+}
